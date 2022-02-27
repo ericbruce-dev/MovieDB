4e11b7b4f63626146220a68b3a327440141bdc39
Eric Bruce <35344980+ericbruce-dev@users.noreply.github.com>
2022-02-27 00:48:59 -0500

    Initial commit

---
+++ MovieDB/Models/Settings/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieDB.Models.Settings
+{
+    public class AppSettings
+    {
+        public MovieSettings MovieDbSettings { get; set; }
+        public TMDBSettings TBDBSettings { get; set; }
+    }
+}
---
+++ MovieDB/Models/Settings/MovieSettings.cs
@@ -0,0 +1,26 @@
+﻿namespace MovieDB.Models.Settings
+{
+    public class MovieSettings
+    {
+        public string TmdbApiKey { get; set; }
+        public string DefaultBackdropSize { get; set; }
+        public string DefaultPosterSize { get; set; }
+        public string DefaultYouTubeKey { get; set; }
+        public string DefaultCastImage { get; set; }
+        public DefaultCollection DefaultCollection { get; set; }
+        public DefaultCredentials DefaultCredentials { get; set; }
+    }
+
+    public class DefaultCollection
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class DefaultCredentials
+    {
+        public string Role { get; set; }
+        public string Email { get; set; }
+        public string Passoword { get; set; }
+    }
+}
