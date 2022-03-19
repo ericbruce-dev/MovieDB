2c01a8819020db741d835868f1f4197664a864d4
Eric Bruce <35344980+ericbruce-dev@users.noreply.github.com>
2022-03-19 14:19:44 -0400

    Actors Controller

---
+++ MovieDB/Controllers/ActorsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieDB.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieDB.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IRemoteMovieService _tmdbMovieService;
+        private readonly IDataMappingService _mappingService;
+
+        public ActorsController(IRemoteMovieService tmdbMovieService, IDataMappingService mappingService)
+        {
+            _tmdbMovieService = tmdbMovieService;
+            _mappingService = mappingService;
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var actor = await _tmdbMovieService.ActorDetailAsync(id);
+            actor = _mappingService.MapActorDetail(actor);
+
+            return View(actor);
+        }
+    }
+}
